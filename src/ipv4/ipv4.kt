260ed98963ba9a1a18a103a91988c9ae34bd361b
ahmed samy <ahmbdelkhalek@gmail.com>
2025-04-04 22:08:15 +0200

    removing all comments

--- src/ipv4/ipv4.kt
+++ src/ipv4/ipv4.kt
@@ -2,17 +2,12 @@ package ipv4
 
 
 fun isIpCorrect(ip:String):Boolean{
-    // Checking if the number of parts is 4 or not
     val parts = ip.split(".")
     if (parts.size != 4) return false
 
     parts.forEach { part ->
 
-        // Ensure that each segment is not blank
         if (part.isBlank()) return false
-
-
-        // Checking if the number has leading zero or not
         if (part.length > 1 && part.startsWith("0")) return false
         val number = part.toIntOrNull()
         if (number == null) {
